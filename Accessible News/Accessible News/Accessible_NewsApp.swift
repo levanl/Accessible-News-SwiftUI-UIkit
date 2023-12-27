f5754a23bf3fb07e4be2a903fe0bc4ee49c39c75
Levan Loladze <levan.loladzew@gmail.com>
2023-12-27 18:09:33 +0400

    first commit

---
+++ Accessible News/Accessible News/Accessible_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Accessible_NewsApp.swift
+//  Accessible News
+//
+//  Created by Levan Loladze on 27.12.23.
+//
+
+import SwiftUI
+
+@main
+struct Accessible_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
